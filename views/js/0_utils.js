f5c57d1e2fefab52e6159c66cefc31e9575e1262
BrazeDaGreat <braze.da.great@outlook.com>
2023-07-20 23:31:58 +0500

    Template Data

--- views/js/0_utils.js
+++ views/js/0_utils.js
@@ -56,43 +56,6 @@ function setElement(id, html) {
 }
 
 /**
- * Loads the profile data for the user with the specified ID.
- * @returns {Promise<void>} - A Promise that resolves when the profile data is loaded.
- */
-async function loadData() {
-    const id = sessionStorage.getItem("profile");
-    const t1 = await node.call('read', 't1', { ID: id });
-    console.log(t1);
-    const profileData = t1[0];
-  
-    const dob = `${formatTime(profileData["DOB"])} (${formatAge(profileData["DOB"])})`;
-    const doe = `${formatTime(profileData["DOE"])} (${formatAge(profileData["DOE"])})`;
-  
-    setElement("profileOverview", `
-      <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-      <h2>${profileData["Rank"]} ${profileData["Name"]}</h2>
-      <h3>ID#. ${profileData["ID"]}</h3>
-    `);
-  
-    setValue("valueId", profileData["ID"]);
-    setValue("valueBty", profileData["BTY"]);
-    setValue("valueName", profileData["Name"]);
-    setValue("valueLocation", profileData["Current Address"]);
-    setValue("valueDOB", dob);
-    setValue("valueDOE", doe);
-    // setValue("valueNote", profileData["Note"]);
-    setValue("valueRank", profileData["Rank"]);
-    setValue("valuePerma", profileData["Permanent Address"]);
-    setValue("valueNo", profileData["Phone No#"]);
-  
-    displayTable('t2', 't2', { ID: id }, ["ID"], "class='table table-bordered'");
-    displayTable('t3', 't3', { ID: id }, ["ID"], "class='table table-bordered'");
-    displayTable('t4', 't4', { ID: id }, ["ID"], "class='table table-bordered'");
-    displayTable('t5', 't5', { ID: id }, ["ID"], "class='table table-bordered'", false);
-    displayTable('t6', 't6', { ID: id }, ["ID"], "class='table table-bordered'");
-}
-
-/**
  * Formats a Unix timestamp to a date string.
  * @param {number|string} unix - The Unix timestamp.
  * @param {boolean} [ymd=false] - Whether to format the date as "YYYY-MM-DD".
@@ -140,4 +103,74 @@ function formatAge(dob) {
   }
 
   return `${ageYears}y ${ageMonths}m ${ageDays}d`;
+}
+
+/**
+ * Generates statistics for leave data and displays them.
+ * @param {DataObject[]} obj - The leave data.
+ */
+function generateLeaveStats(obj) {
+  const lastLocations = {};
+  for (const { ID, Location } of obj) {
+    lastLocations[ID] = Location;
+  }
+
+  const countByLocation = {};
+  for (const location of Object.values(lastLocations)) {
+    countByLocation[location] = (countByLocation[location] || 0) + 1;
+  }
+
+  const leaveStatsElement = document.getElementById("leaveStats");
+  const html = [];
+  if (leaveStatsElement) {
+    for (const [location, count] of Object.entries(countByLocation)) {
+      html.push(`
+        <h6 class="text-success pt-1 fw-bold">${count}</h6>
+        <span class="text-muted small pt-2 ps-1">@ <b>${location}</b></span>
+      `);
+    }
+    leaveStatsElement.outerHTML = html.join("");
+  }
+}
+
+/**
+ * Renders the page and displays various statistics and tables.
+ * @returns {Promise<void>} - A Promise that resolves when the rendering is complete.
+ */
+async function render() {
+  const leaveData = await node.call('read', 't6');
+  generateLeaveStats(leaveData);
+
+  const userData = await node.call('read', 't1');
+  const courseData = await node.call('read', 't2');
+  setElement("userStats", `
+    <div class="d-flex align-items-center">
+      <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+        <i class="bi bi-people-fill"></i>
+      </div>
+      <h6 class="text-primary pt-1 fw-bold ps-3">${userData.length}</h6>
+      <span class="text-muted small pt-2 ps-1">Users</span>
+    </div>
+    <div class="d-flex align-items-center">
+      <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+        <i class="bi bi-book-half"></i>
+      </div>
+      <h6 class="text-success pt-1 ps-3 fw-bold">${courseData.length}</h6>
+      <span class="text-muted small pt-2 ps-1">Courses</span>
+    </div>
+  `);
+
+  displayTable('t1', 'tabledata', {}, ["Permanent Address", "Current Address", "BTY"]);
+  document.getElementById("userEmail").outerHTML = sessionStorage.getItem('email');
+
+  if (sessionStorage.getItem('email') === "admin@ex.com") {
+    setElement("adminDashboard",
+      `<li class="nav-item">
+        <a class="nav-link collapsed" href="./admin.html">
+          <i class="bi bi-hammer"></i>
+          <span>Admin Dashboard</span>
+        </a>
+      </li>`
+    );
+  }
 }
